d64257cc3ffea7dfe1292f159b08945b1a16cfe9
AgarwalSaurav <agr.saurav1@gmail.com>
2017-06-09 17:37:48 -0400

    Added export

--- sfm_script.sh
+++ sfm_script.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
-SFM_DIR="/opt/openSFM/OpenSfM"
-DATASET_DIR="$SFM_DIR/data/dataset1_gdrive"
+export SFM_DIR="/opt/openSFM/OpenSfM"
+export DATASET_DIR="$SFM_DIR/data/dataset1_gdrive"
 cd $SFM_DIR
 ./bin/run_all $DATASET_DIR
 ./bin/opensfm undistort $DATASET_DIR
