8a2234e8805f50a3dd0e2f752c89edf5fe842200
AgarwalSaurav <agr.saurav1@gmail.com>
2017-06-09 16:40:38 -0400

    sfm local script

--- sfm_script.sh
+++ sfm_script.sh
@@ -1,16 +1,6 @@
 #! /bin/bash
-# ===== PBS OPTIONS =====
-### Set the job name
-#PBS -N "JobSFM"
-### Run in the queue named "copperhead"
-#PBS -q copperhead
-### Specify the number of cpus for your job.
-#PBS -l nodes=1:ppn=8:gpus=1
-#PBS -l mem=64GB
-#PBS -l walltime=24:00:00
-# ==== Main ======
-module load intel openmpi
-module load cuda
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/users/sagarw10/opt/CGAL-4.9/lib
-
-/users/sagarw10/opt/openMVS/openMVS_build/bin/DensifyPointCloud /users/sagarw10/opt/openSFM/OpenSfM/data/dataset1_gdrive/openmvs/scene.mvs
+cd /opt/openSFM/OpenSfM/
+./bin/run_all data/dataset
+./bin/opensfm undistort path_to_data
+./bin/opensfm compute_depthmaps path_to_data
+./bin/opensfm export_openmvs path_to_data
