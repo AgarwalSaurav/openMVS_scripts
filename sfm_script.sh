6a8cce37965547c2684b3451df30dc3dff503832
AgarwalSaurav <agr.saurav1@gmail.com>
2017-06-10 15:37:40 -0400

    Added timing variables

--- sfm_script.sh
+++ sfm_script.sh
@@ -2,7 +2,19 @@
 export SFM_DIR="/opt/openSFM/OpenSfM"
 export DATASET_DIR="$SFM_DIR/data/dataset1_gdrive"
 cd $SFM_DIR
+SECONDS=0
 ./bin/run_all $DATASET_DIR
+runall_time=$SECONDS
 ./bin/opensfm undistort $DATASET_DIR
+undistort_time=`expr runall_time - $SECONDS`
 ./bin/opensfm compute_depthmaps $DATASET_DIR
+depthmaps_time=`expr $SECONDS - $undistort_time`
 ./bin/opensfm export_openmvs $DATASET_DIR
+export_time=`expr $SECONDS - $depthmaps_time`
+
+echo "Dataset path: $DATASET_DIR"
+echo "Time taken for run_all: $runall_time"
+echo "Time taken for undistort: $undistort_time"
+echo "Time taken for depthmamps: $depthmaps_time"
+echo "Time taken for exporting: $export_time"
+echo "Total time taken: $SECONDS"
